96ad7f4623804968f58c06420d359a8b841979cf
fanuelal <fanuelalmawstop.com>
2022-01-13 08:05:48 -0800

    pack web

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo"""
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """the method that pack the folder with date"""
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    fileName = "versions/web_static_{}.tgz".format(date)
+    path = local("sudo tar -cvzf {} web_static".format(fileName))
+    if path.secceeded:
+        return path
+    else:
+        return None
