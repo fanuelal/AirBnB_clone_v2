487eaaa683dfdd0bebddb85160c8b8e1b431ac5a
fanuelal <fanuelalmawstop.com>
2022-01-13 08:17:48 -0800

    pack web

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,8 +10,8 @@ def do_pack():
     local("sudo mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     fileName = "versions/web_static_{}.tgz".format(date)
-    path = local("sudo tar -cvzf {} web_static".format(fileName))
-    if path.secceeded:
+    path = local("sudo tar -cvzf {} web_static".format(fileName), capture=True)
+    if path.succeeded:
         return path
     else:
         return None
