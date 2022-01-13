c66a4db0a5be047ec678fb16b1ad959877a628e8
fanuelal <fanuelalmawstop.com>
2022-01-13 07:18:22 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,22 @@
-0-setup_web_static.sh#!/usr/bin/env bash
+#!/usr/bin/env bash
 #sets up your web servers for the deployment of web_static
 apt-get -y update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 
-mkdir -p /data/
 mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-cat 'echo "<html>
+
+echo '<html>
   <head>
   </head>
   <body>
     Hellow world!
   </body>
-</html>" ' > /data/web_static/releases/test/index.html
+</html>'  > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
+chown -hR ubuntu:ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
-
 exit 0
