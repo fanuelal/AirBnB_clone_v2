2b85cd218108b826db85443583519d4414531d85
fanuelal <fanuelalmawstop.com>
2022-01-16 00:32:07 -0800

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo"""
+"""Fabric script that generates a
+.tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo"""
 
 from fabric.api import *
 from datetime import datetime
